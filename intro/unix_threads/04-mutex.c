6134cbab128a01fbbe05865f464385bb100d6fda
Tat Hoang Nguyen <tat-nguy@c1r3p7.42perpignan.fr>
2025-03-20 14:33:00 +0100

    philo

--- intro/unix_threads/04-mutex.c
+++ intro/unix_threads/04-mutex.c
@@ -3,11 +3,16 @@
 #include <pthread.h>
 
 int mails = 0;
+pthread_mutex_t	mutex;
 
 void	*routine()
 {
-	for (int i = 0; i < 100000; i++)
+	for (int i = 0; i < 20000000; i++)
+	{
+		pthread_mutex_lock(&mutex);
 		mails++;
+		pthread_mutex_unlock(&mutex);
+	}
 	return (NULL);
 }
 
@@ -15,15 +20,27 @@ int	main(int argc, char **argv)
 {
 	pthread_t	t1;
 	pthread_t	t2;
+	pthread_t	t3;
+	pthread_t	t4;
 
+	pthread_mutex_init(&mutex, NULL);
 	if (pthread_create(&t1, NULL, routine, NULL) != 0)
 		return (EXIT_FAILURE);
 	if (pthread_create(&t2, NULL, routine, NULL) != 0)
+		return (EXIT_FAILURE);
+	if (pthread_create(&t3, NULL, routine, NULL) != 0)
+		return (EXIT_FAILURE);
+	if (pthread_create(&t4, NULL, routine, NULL) != 0)
 		return (EXIT_FAILURE);
 	if (pthread_join(t1, NULL) != 0)
 		return (EXIT_FAILURE);
 	if (pthread_join(t2, NULL) != 0)
 		return (EXIT_FAILURE);
+	if (pthread_join(t3, NULL) != 0)
+		return (EXIT_FAILURE);
+	if (pthread_join(t4, NULL) != 0)
+		return (EXIT_FAILURE);
+	pthread_mutex_destroy(&mutex);
 	printf("Number of mails: %i\n", mails);
 	return (0);
 }
